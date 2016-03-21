e98a3e97fe7753342228ffbe5cc6a22a5e158911
John Goulah <jgoulah@gmail.com>
2016-03-20 22:35:41 -0400

    turn off the functionality that modifies server uri

--- lib/chef/provisioning/docker_driver/docker_transport.rb
+++ lib/chef/provisioning/docker_driver/docker_transport.rb
@@ -150,8 +150,8 @@ module DockerDriver
         old_uri = uri.dup
         # Find out our external network address of the URL and report it
         # to the container in case it has no DNS (often the case).
-        uri.scheme = 'http' if 'chefzero' == uri.scheme && uri.host == 'localhost'
-        uri.host = Socket.getaddrinfo(uri.host, uri.scheme, nil, :STREAM)[0][3]
+        #uri.scheme = 'http' if 'chefzero' == uri.scheme && uri.host == 'localhost'
+        #uri.host = Socket.getaddrinfo(uri.host, uri.scheme, nil, :STREAM)[0][3]
         Chef::Log.debug("Looked up IP address of #{old_uri} and modified URL to point at it: #{uri}")
       end
 
